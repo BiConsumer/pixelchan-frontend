50b75d281e6b03efdf9daf7c6c7189c110c79330
BiConsumer <insanelydeveloper@gmail.com>
2023-03-12 00:25:42 -0500

    make environment work

--- src/app/core/service/rest.service.ts
+++ src/app/core/service/rest.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {Model} from "../model/model";
 import {Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
-import {environment} from "../../environments/environment";
+import {environment} from "../../../environments/environment";
 
 const URL = environment.backend
 
